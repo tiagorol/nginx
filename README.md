04e93ef47565b1475f4a824b747e6c1791f368b8
Tiago Rolim <tiago.rol@gmail.com>
2015-07-18 12:21:21 +0000

    config

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Instação do Nginx
 
-Exemplo: docker run -d -p 80:80 tiagorol/nginx
+Exemplo: docker run -d -p 80:80 -e HOST_WP1=192.168.0.14 -e HOST_WP2=192.168.0.14 tiagorol/nginx
--- run.sh
+++ run.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 set -e
 
+if [ ! -z $HOST_WP1 ];then
+   sed -i "s/#server HOST_WP1 weight=1;/server $HOST_WP1 weight=1;/" /etc/nginx/nginx.conf
+fi
+
+if [ ! -z $HOST_WP2 ];then
+   sed -i "s/#server HOST_WP2 weight=1;/server $HOST_WP2 weight=1;/" /etc/nginx/nginx.conf
+fi
+
+if [ ! -z $HOST_WP3 ];then
+   sed -i "s/#server HOST_WP3 weight=1;/server $HOST_WP3 weight=1;/" /etc/nginx/nginx.conf
+fi
+
+if [ ! -z $HOST_WP4 ];then
+   sed -i "s/#server HOST_WP4 weight=1;/server $HOST_WP4 weight=1;/" /etc/nginx/nginx.conf
+fi
+
+if [ ! -z $HOST_WP5 ];then
+   sed -i "s/#server HOST_WP5 weight=1;/server $HOST_WP5 weight=1;/" /etc/nginx/nginx.conf
+fi
+
+
 exec nginx -g "daemon off;"
